43681747a35eb52dc7afa5f70a36b79d8bfd0443
oliverochman <oliver.ochman@hotmail.com>
2019-12-02 15:11:04 +0100

    generate Article model and write unit specs for it

---
+++ spec/models/article_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Article, type: :model do
+  describe "DB columns" do
+    it { is_expected.to have_db_column :title }
+    it { is_expected.to have_db_column :content }
+  end
+  
+  describe "Validations" do
+    it { is_expected.to validate_presence_of :title }
+    it { is_expected.to validate_presence_of :content }
+  end
+
+  it "has a valid Factory" do
+    expect(create(:article)).to be_valid
+  end
+end
