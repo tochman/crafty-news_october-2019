7410833500fbff42e66668c15686d2f1b55edff9
oliverochman <oliver.ochman@hotmail.com>
2019-12-03 09:30:52 +0100

    add new feature test with steps

--- features/step_definitions/assertions.rb
+++ features/step_definitions/assertions.rb
@@ -1,3 +1,7 @@
 Then("I should see {string}") do |content|
   expect(page).to have_text content
 end
+
+Then("I should be on the {string} page") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -3,3 +3,11 @@
 When('I am on the index page') do
   visit root_path
 end
+
+When("I fill in {string} with {string}") do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click on {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
