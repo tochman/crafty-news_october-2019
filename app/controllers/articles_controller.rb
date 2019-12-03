81c95ec11e3582dec66a62a15ea3a0de2ef3727d
oliverochman <oliver.ochman@hotmail.com>
2019-12-03 10:03:27 +0100

    add conditional for article creation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,20 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def create 
+    article = Article.create(article_params)
+    
+    if article.persisted? 
+      redirect_to article
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
 end
