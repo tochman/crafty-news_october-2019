2285960d674b27391362c6af5d4f4d47c081189e
oliverochman <oliver.ochman@hotmail.com>
2019-12-03 10:10:23 +0100

    add show action to articles controller with template

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root controller: :articles, action: :index
-  resources :articles, only: [:new, :create]
+  resources :articles, only: [:new, :create, :show]
 end
