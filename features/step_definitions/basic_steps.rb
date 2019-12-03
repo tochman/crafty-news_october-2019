72b4f60e47e75fb22afc4842a216dd4b87ebe30b
oliverochman <oliver.ochman@hotmail.com>
2019-12-03 09:36:43 +0100

    add empty "New Article" link

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -8,6 +8,6 @@ When("I fill in {string} with {string}") do |string, string2|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-When("I click on {string}") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+When("I click on {string}") do |element|
+  click_on element
 end
