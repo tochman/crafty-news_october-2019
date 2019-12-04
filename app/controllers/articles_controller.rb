61b29d643fe319fc915d4ddfa63d79e0f7f018f6
Thomas <thomas@craftacademy.se>
2019-12-04 15:25:03 +0100

    adds new/create actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,18 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.create(article_params)
+    render :new unless @article.persisted?
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  resources :articles, only: %i[new create]
   root controller: :articles, action: :index
 end
