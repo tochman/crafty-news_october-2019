6898e1bb493aa4b1c415e03a30bcd8c2219f3c8a
oliverochman <oliver.ochman@hotmail.com>
2019-12-12 09:54:01 +0100

    add comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,7 @@
+class CommentsController < ApplicationController
+  def create
+    
+    binding.pry
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root controller: :articles, action: :index
-  resources :articles, only: [:new, :create, :show]
+  resources :articles, only: [:new, :create, :show] do
+    resources :comments, only: [:create]
+  end
 end
