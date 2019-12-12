34fd37b607b157a57ea84172d9c4279794d3c876
oliverochman <oliver.ochman@hotmail.com>
2019-12-12 09:59:47 +0100

    add logic to create action for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,20 @@
 class CommentsController < ApplicationController
   def create
+    article = Article.find(params[:article_id])
     
-    binding.pry
-    
+    comment = article.comments.create(body: comment_params[:body])
+
+    if comment.persisted?
+      flash[:notice] = "Commented"
+    else
+      flash[:alert] = "Something went wrong"
+    end
+    redirect_to article_path(article)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
   end
 end
